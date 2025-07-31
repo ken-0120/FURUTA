92145271ff5c19274c221b4a6ecc53fcc129844a
kenji <kenji.prutas800@gmail.com>
2025-07-31 12:31:26 +0900

    asd

--- js/scroll-fade.js
+++ js/scroll-fade.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const scrollElements = document.querySelectorAll(".scroll_up, .scroll_left, .scroll_right");
-
-  const scrollAnimation = () => {
-    scrollElements.forEach((el) => {
-      const triggerMargin = 200;
-      const rect = el.getBoundingClientRect();
-      if (window.innerHeight > rect.top + triggerMargin) {
-        el.classList.add("on");
-      }
-    });
-  };
-
-  window.addEventListener("load", scrollAnimation);
-  window.addEventListener("scroll", scrollAnimation);
+    const scrollElements = document.querySelectorAll(".scroll_up, .scroll_left, .scroll_right");
+    const scrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            const triggerMargin = 200;
+            const rect = el.getBoundingClientRect();
+            if (window.innerHeight > rect.top + triggerMargin) {
+                el.classList.add("on");
+            }
+        });
+    };
+    window.addEventListener("load", scrollAnimation);
+    window.addEventListener("scroll", scrollAnimation);
 });
