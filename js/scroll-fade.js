d7007f1ee7f4113cb79fc5ec4789fcde7d78302f
ken-0120 <kenji.prutas800@gmail.com>
2025-08-03 19:25:18 +0900

    d

--- js/scroll-fade.js
+++ js/scroll-fade.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const scrollElements = document.querySelectorAll(".scroll_up, .scroll_left, .scroll_right");
     const scrollAnimation = () => {
         scrollElements.forEach((el) => {
-            const triggerMargin = 200;
+            const triggerMargin = 400;
             const rect = el.getBoundingClientRect();
             if (window.innerHeight > rect.top + triggerMargin) {
                 el.classList.add("on");
