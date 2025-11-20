59db3b5ea8580564b7af0abfbc7477e1db1a95ec
ken-0120 <167536578+ken-0120@users.noreply.github.com>
2025-11-20 14:00:27 +0900

    asd

--- js/hambarger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-const hamback = document.querySelector('.hamback');
-hamback.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
