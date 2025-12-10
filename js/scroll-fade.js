31815d6eae7520c28df9c012093d758de00c04e7
ken-0120 <167536578+ken-0120@users.noreply.github.com>
2025-12-10 10:47:43 +0900

    asd

--- js/scroll-fade.js
+++ js/scroll-fade.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const scrollElements = document.querySelectorAll(".scroll_up, .scroll_left, .scroll_right, .scroll_circlar, .scroll_line");
     const scrollAnimation = () => {
         scrollElements.forEach((el) => {
-            const triggerMargin = 600;
+            const triggerMargin = 300;
             const rect = el.getBoundingClientRect();
             if (window.innerHeight > rect.top + triggerMargin) {
                 el.classList.add("on");
